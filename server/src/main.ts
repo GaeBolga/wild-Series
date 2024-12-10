85e528490605141c324881795bd36a52270ac29d
GaeBolga <benjamin.lefort@mail.com>
2024-12-10 16:13:23 +0100

    first commit

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,6 +4,7 @@ import "dotenv/config";
 // Check database connection
 // Note: This is optional and can be removed if the database connection
 // is not required when starting the application
+
 import "../database/checkConnection";
 
 // Import the Express application from ./app
@@ -12,6 +13,12 @@ import app from "./app";
 // Get the port from the environment variables
 const port = process.env.APP_PORT;
 
+import type { RequestHandler } from "express";
+
+const sayWelcome: RequestHandler = (req, res) => {
+  res.send("Welcome to Wild Series !");
+};
+app.get("/", sayWelcome);
 // Start the server and listen on the specified port
 app
   .listen(port, () => {
